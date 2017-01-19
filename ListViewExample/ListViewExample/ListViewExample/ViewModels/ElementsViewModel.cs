1f29f016e34f75882c09c10e31a9d44f0cb8066e
Juan Ramon <juan.santiago2@hotmail.com>
2017-01-19 14:52:45 -0600

    Creación de Lista personalizada en Xamarin Forms

---
+++ ListViewExample/ListViewExample/ListViewExample/ViewModels/ElementsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewExample.ViewModels
+{
+    public class ElementsViewModel
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ ListViewExample/ListViewExample/ListViewExample/Views/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ListViewExample.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace ListViewExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<ElementsViewModel> elements { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            elements = new ObservableCollection<ElementsViewModel>();
+            elements.Add(new ElementsViewModel
+            {
+                Image = "icon.png",
+                Name = "José",
+                Type = "Persona"
+            });
+            elements.Add(new ElementsViewModel
+            {
+                Image = "icon.png",
+                Name = "Andres",
+                Type = "Persona"
+            });
+            elements.Add(new ElementsViewModel
+            {
+                Image = "icon.png",
+                Name = "Perro",
+                Type = "Animal"
+            });
+            elements.Add(new ElementsViewModel
+            {
+                Image = "icon.png",
+                Name = "Gato",
+                Type = "Animal"
+            });
+            listView.ItemsSource = elements;
+        }
+    }
+}
